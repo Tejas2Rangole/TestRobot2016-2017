6c166f7ab77eddea233870e01380af1a9315d4f5
gregoryquick <gregory@quicks.me>
2016-11-01 20:11:01 -0700

    Use of functions in subsystems typo
    Fixed missaranged words in comment

--- src/org/usfirst/frc/team948/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team948/robot/subsystems/ExampleSubsystem.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ExampleSubsystem extends Subsystem {
     
     // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    // here. Call Commands from these.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.

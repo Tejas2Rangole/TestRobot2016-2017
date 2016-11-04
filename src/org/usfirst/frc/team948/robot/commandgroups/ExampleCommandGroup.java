b61002419087a054d219dcce25744aa02d36b9a4
gregoryquick <gregory@quicks.me>
2016-11-03 19:24:05 -0700

    Command group additions
    -Added some extra desctiptive text to ExampleCommandGroup

--- src/org/usfirst/frc/team948/robot/commandgroups/ExampleCommandGroup.java
+++ src/org/usfirst/frc/team948/robot/commandgroups/ExampleCommandGroup.java
@@ -11,12 +11,14 @@ import org.usfirst.frc.team948.robot.Robot;
 
 //WIP look at at your own risk, possibly totaly wrong
 public class ExampleCommandGroup extends CommandGroup {
+	//This is run when a new command group is created, no other specific functions in command groups are important to the WPI libraries
 	public ExampleCommandGroup(){
 		//Uses addSequential to schedule one sequential running of ExampleCommand
 		addSequential(new ExampleCommand()); //Command 1
 		//Uses addParallel and add Sequential to schedule the parellel running of two more ExampleCommands after command one
 		addParallel(new ExampleCommand()); //Command 2
 		addSequential(new ExampleCommand()); //Command 3
+		//After the last command (or all of the last parrallel batch of commands) are finished running the command group is done
 	}
 
 }

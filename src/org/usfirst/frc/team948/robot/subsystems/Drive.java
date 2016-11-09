440e4d24f4a067fe774ec473ca32871fb4a7afc8
Dudedmn <daniely82911@gmail.com>
2016-11-08 20:15:02 -0800

    Update Drive.java
    Updated drive from old code to appropriate status of current Robot

--- src/org/usfirst/frc/team948/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team948/robot/subsystems/Drive.java
@@ -1,24 +1,25 @@
 package org.usfirst.frc.team948.robot.subsystems;
-
+// Placed notified on what hasn't actually been coded yet: All start with /** and have [Non-implemented] in the comment somewhere
+//What has been implemented: Commandbase for drive, manual drive.
 import org.usfirst.frc.team948.robot.RobotMap;
 import org.usfirst.frc.team948.robot.commands.CommandBase;
 import org.usfirst.frc.team948.robot.commands.ManualDrive;
 import org.usfirst.frc.team948.robot.utilities.MathHelper;
 import org.usfirst.frc.team948.robot.utilities.PreferenceKeys;
 
-import edu.wpi.first.wpilibj.PIDController;
+/**import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSource; 
+[Non-implemented]**/
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+[Non-implemented]**/
 
 
-/**
- *
- */
 
-public class Drive extends Subsystem implements PIDOutput {
-	private static final int REQUIRED_CYCLES_ON_TARGET = 5;//NEED TO CHECK/CHANGE LATER
+public class Drive extends Subsystem /** implements PIDOutput [Non-implemented]**/{
+	/**private static final int REQUIRED_CYCLES_ON_TARGET = 5;//NEED TO CHECK/CHANGE LATER
 	private static final double TURN_TO_HEADING_P = 0.039; //NEED TO CHECK/CHANGE LATER
 	private static final double TURN_TO_HEADING_I = 0.0056; //NEED TO CHECK/CHANGE LATER
 	private static final double TURN_TO_HEADING_D = 0.06825; //NEED TO CHECK/CHANGE LATER
@@ -35,7 +36,8 @@ public class Drive extends Subsystem implements PIDOutput {
 
 	public PIDController drivePID;
 	private int cyclesOnTarget;
-
+	
+	[Non-implemented]**/
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
@@ -48,24 +50,26 @@ public class Drive extends Subsystem implements PIDOutput {
 	}
 
 	
-
+/**
 	public void rawTankDrive(double leftPower, double rightPower) {
-
+possible name changes here for motors
 		RobotMap.motorFrontLeft.set(leftPower);
 		RobotMap.motorFrontRight.set(-1 * rightPower);
 		RobotMap.motorBackLeft.set(leftPower);
 		RobotMap.motorBackRight.set(-1 * rightPower);
 
 	}
-
-	public void rawStop() {
+	
+	[Non-implemented]**/
+	/**public void rawStop() {
 		RobotMap.motorBackLeft.disable();
 		RobotMap.motorBackRight.disable();
 		RobotMap.motorFrontLeft.disable();
 		RobotMap.motorFrontRight.disable();
 	}
+	[Non-implemented]**/
 
-	public void setDesiredHeadingFromGyro() {
+/**	public void setDesiredHeadingFromGyro() {
 		setDesiredHeading(RobotMap.driveGyro.getAngle());
 	}
 	
@@ -76,21 +80,24 @@ public class Drive extends Subsystem implements PIDOutput {
 	public void setDesiredHeading(double angle) {
 		desiredHeading = angle;
 	}
+	[Non-implemented] **/
 	
-	public double drivePIDInit(double p, double i, double d, double maxOutput) {
+	/**public double drivePIDInit(double p, double i, double d, double maxOutput) {
 		drivePID = new PIDController(p, i, d, (PIDSource)RobotMap.driveGyro, this);
 		drivePID.reset();
 		drivePID.setOutputRange(-Math.abs(maxOutput), Math.abs(maxOutput));
 		PIDOutput = 0;
 		drivePID.enable();
 		System.out.println("Drive P:" + p + " I:" + i + " D:" + d);
-		return RobotMap.driveGyro.getAngle();
+		return RobotMap.driveGyro.getAngle();		
 	}
+	[Non-implemented]**/
 
-	@Override
+	/**@Override
 	public void pidWrite(double arg0) {
 		PIDOutput = arg0;
 	}
+	[Non-implemented]**/
 	
 	public double driveOnHeadingInit(double maxOutput){
 		return drivePIDInit(
@@ -101,7 +108,7 @@ public class Drive extends Subsystem implements PIDOutput {
 
 	}
 	public void driveOnHeading(double power, double heading) {
-		drivePID.setSetpoint(heading);
+		/**drivePID.setSetpoint(heading);
 
 		double error = heading - RobotMap.driveGyro.getAngle();
 		double outputRange = MathHelper.clamp(PID_MIN_OUTPUT
@@ -124,14 +131,17 @@ public class Drive extends Subsystem implements PIDOutput {
 		}
 		SmartDashboard.putNumber("Left Drive Straight output", leftPower);
 		SmartDashboard.putNumber("Right Drive Straight output", rightPower);
+		[Non-implemented]**/
 		rawTankDrive(leftPower, rightPower);
 	}
 	public void driveOnHeadingEnd(){ 
 		rawStop();
+		/**
 		drivePID.reset();
 		PIDOutput = 0;
+		[Non-implemented]**/
 	}
-	
+	/**
 	public void turnToHeadingInit(double finalHeading, double tolerance, double maxOutput) {
 		cyclesOnTarget = getRequiredCyclesOnTarget();
 		drivePIDInit(
@@ -145,7 +155,9 @@ public class Drive extends Subsystem implements PIDOutput {
 		prevError = 0;
 		counter = 0;
 	}
-	public void turnToHeading(double finalHeading, double power){
+	[Non-implemented]**/
+	
+	/**public void turnToHeading(double finalHeading, double power){
 //		drivePID.setSetpoint(finalHeading);
 		double currentError = drivePID.getError();
 		SmartDashboard.putNumber("Turn PIDOutput", PIDOutput);
@@ -162,14 +174,14 @@ public class Drive extends Subsystem implements PIDOutput {
 		SmartDashboard.putNumber("Turn Heading Cross Setpoint", counter);
 		rawTankDrive(revisedPower, -revisedPower);
 	}
-	
-	public void turnToHeadingEnd(double newHeading){
+	[Non-implemented]**/
+	/**public void turnToHeadingEnd(double newHeading){
 		setDesiredHeading(newHeading); 
 		drivePID.reset();
 		PIDOutput = 0;
 	}
-	
-	public boolean turnToHeadingComplete(double tolerance){
+	[Non-implemented]**/
+	/**public boolean turnToHeadingComplete(double tolerance){
 		boolean onTarget = Math.abs(drivePID.getError()) < tolerance;
 		SmartDashboard.putNumber("Turn Error", drivePID.getError());
 		
@@ -185,13 +197,13 @@ public class Drive extends Subsystem implements PIDOutput {
 		
 		return cyclesOnTarget >= getRequiredCyclesOnTarget();
 	}
+	[Non-implemented]**/
 	
-	
-	public int getRequiredCyclesOnTarget(){
+	/**public int getRequiredCyclesOnTarget(){
 		return REQUIRED_CYCLES_ON_TARGET;
 	}
 	
-	
+	[Non-implemented]**/
 		
 		
 	

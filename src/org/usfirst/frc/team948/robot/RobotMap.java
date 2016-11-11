4bbd117edc42f806e1f0e1af7d3765b9ba200198
gregoryquick <gregory@quicks.me>
2016-11-10 18:55:02 -0800

    Imported Libraries to robotMap

--- src/org/usfirst/frc/team948/robot/RobotMap.java
+++ src/org/usfirst/frc/team948/robot/RobotMap.java
@@ -1,4 +1,6 @@
 package org.usfirst.frc.team948.robot;
+import edu.wpi.first.wpilibj.*;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
